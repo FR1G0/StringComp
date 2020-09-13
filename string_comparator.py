f7ce8c3c141eccfeda77111b533d592e4223aec0
PotatoMaster69 <npasett@gmail.com>
2020-09-13 17:31:20 +0200

    Made the "string_comparator" algoritmh more compact

--- string_comparator.py
+++ string_comparator.py
@@ -1,51 +1,41 @@
 def __main__():
     '''Algoritmh to compare 2 strings'''
-    #Define the strings to be compared
+    #Define the strings
     string1 = "This is the first string, it's a very simple string and has nothing special"
     string2 = "This is the second string, it's a very simple string as well and has nothing special"
-    #Clean the strings
-    print(f"BEFORE:\n{string1}\n{string2}\n\nAFTER:")
-    string1 = stabilise_patient(f"{string1} ")
-    string2 = stabilise_patient(f"{string2} ")
-    print(f"{string1}\n{string2}\n")
     #Call main function
     compare(string1, string2)
 
-def _get_lenght(string1, string2):
-    '''Get the max lenght between 2 strings'''
-    len1, len2 = len(string1.split()), len(string2.split())
-    if len1 > len2:
-        return len1
+def _max_lenght(string1, string2):
+    '''Get the max lenght between the 2 strings'''
+    if len(string1.split()) > len(string2.split()):
+        return len(string1.split())
     else:
-        return len2
+        return len(string2.split())
 
-def stabilise_patient(string):
+def _normalise(string):
     '''Make string following standarts'''
-    #Define the blacklists
-    word_bl = ['a' ,"is", "it", "for",
-               "it's", "an" ,"as", "and",
-               "the", "has", "this",
-               "that", "there"]
-    char_bl = ['.', ',', '?', '!']
+    #Define the blacklist
+    blacklist = [" a " ," is ", " it ", " for ",
+                 " it's ", " an " ," as ", " and ",
+                 " the ", " has ", " this ", " that ",
+                 " there ", '.', ',', '?', '!']
     #Remove unnecessary stuff
-    word, neat = '', ''
-    for entry in string:
-        if entry == ' ':
-            if not word in word_bl:
-                neat += f"{word} "
-            word = ''
-        elif not entry in char_bl:
-             word += entry.lower()
-    return(neat)
+    for entry in blacklist:
+        string = string.replace(entry, ' ')
+    return string
 
 def compare(string1, string2):
     '''Campare word by word 2 strings'''
-    lenght, x = _get_lenght(string1, string2), 0
+    #Clean the strings
+    string1 = _normalise(f"{string1} ".lower())
+    string2 = _normalise(f"{string2} ".lower())
+    #Calculate matches
+    lenght, c = _max_lenght(string1, string2), 0
     for entry in string1.split():
         if entry in string2:
-            x += 1
-    perc = x/lenght*100
-    print(f"MATCHINGS:\t{perc}%\n")
+            c += 1
+    print(f"\n[+] There are {c} matching words out of {lenght}\n")
 
 if __name__ == '__main__':
     __main__()
